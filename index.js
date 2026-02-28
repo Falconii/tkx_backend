afba60db2b9dd2bd7e92d64c2b4875df753318be
Marcos Renato Falconi <falconi.2050@gmail.com>
2026-02-28 16:09:22 -0300

    Versão 28 02 2026

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ const cors = require("cors");
 const express = require("express");
 const app = express();
 
-const PORT =  3000;
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+/*
+  Atualizando Versão 
+  */
 
+const PORT = 3000;
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 
 app.use(express.json());
 /*
@@ -34,26 +39,28 @@ app.use(allowCors);
 app.options('/*', allowCors);
 */
 
-app.use("/api/login", require('./route/loginRoute'));
-app.use("/api/empresa", require('./route/empresaRoute'));
-app.use("/api/usuario", require('./route/usuarioRoute'));
-app.use("/api/grupousuario", require('./route/grupousuarioRoute'));
-app.use("/api/evento", require('./route/eventoRoute'));
-app.use("/api/participante", require('./route/participanteRoute'));
-app.use("/api/inscrito", require('./route/inscritoRoute'));
-app.use("/api/categoria", require('./route/categoriaRoute'));
-app.use("/api/entrega", require('./route/entregaRoute'));
-app.use("/api/link", require('./route/linkRoute'));
-app.use("/api/token", require('./route/tokenRoute'));
-app.use("/api/importacao", require('./route/ImportacaoRoute.js'));
-app.use("/api/cabplanilha", require('./route/cabplanilhaRoute.js'));
-app.use("/api/detplanilha", require('./route/detPlanilhaRoute.js'));
-app.use("/api/uploadfoto", require('./route/fotoRoute.js'));
-app.use("/api/categoriacontador", require('./route/categoriacontadorRoute.js'));
-app.use("/api/categoria_complementar", require('./route/complementar/categoriaRoute'));
-app.use("/api/trocaParticipante", require('./route/trocaparticipanteRoute.js'));
-
+app.use("/api/login", require("./route/loginRoute"));
+app.use("/api/empresa", require("./route/empresaRoute"));
+app.use("/api/usuario", require("./route/usuarioRoute"));
+app.use("/api/grupousuario", require("./route/grupousuarioRoute"));
+app.use("/api/evento", require("./route/eventoRoute"));
+app.use("/api/participante", require("./route/participanteRoute"));
+app.use("/api/inscrito", require("./route/inscritoRoute"));
+app.use("/api/categoria", require("./route/categoriaRoute"));
+app.use("/api/entrega", require("./route/entregaRoute"));
+app.use("/api/link", require("./route/linkRoute"));
+app.use("/api/token", require("./route/tokenRoute"));
+app.use("/api/importacao", require("./route/ImportacaoRoute.js"));
+app.use("/api/cabplanilha", require("./route/cabplanilhaRoute.js"));
+app.use("/api/detplanilha", require("./route/detPlanilhaRoute.js"));
+app.use("/api/uploadfoto", require("./route/fotoRoute.js"));
+app.use("/api/categoriacontador", require("./route/categoriacontadorRoute.js"));
+app.use(
+  "/api/categoria_complementar",
+  require("./route/complementar/categoriaRoute"),
+);
+app.use("/api/trocaParticipante", require("./route/trocaparticipanteRoute.js"));
 
 app.listen(PORT, () => {
-    console.log(`Servidor No Ar. Porta ${PORT}`);
+  console.log(`Servidor No Ar. Porta ${PORT}`);
 });
